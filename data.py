8fa4a3f2e572b617dadd033064cdf995c4c28589
Juan Pablo <if721093@iteso.mx>
2022-09-17 22:49:54 -0500

    cleaning files

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 
 """
 # -- --------------------------------------------------------------------------------------------------- -- #
-# -- project: APT and Roll Model apply to Market Microstructue.                                          -- #
+# -- project: Investment Strategies with ETFs.                                          -- #
 # -- script: data.py : python script for data collection                                                 -- #
-# -- author: JuanPablo2019                                                                               -- #
+# -- author: Bryan Manica & Juan Pablo Ruiz                                                                               -- #
 # -- license: GNU General Public License v3.                                                             -- #
 # -- repository:https://github.com/JuanPablo2019/myst_jprm_lab2.git                                      -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
@@ -12,21 +12,4 @@
 
 #%% libraries
 
-import pandas as pd
-import json
 
-
-
-#%% Orderbook data
-
-f = open('files/orderbooks_05jul21.json')
-orderbooks_data = json.load(f)
-
-#%% orderbook data wranging
-
-ob_data = orderbooks_data['bitfinex']
-
-ob_data = {i_key: i_value for i_key, i_value in ob_data.items() if i_value is not None}
-
-ob_data = {i_ob: pd.DataFrame(ob_data[i_ob])[['bid_size','bid','ask','ask_size']]
-                   if ob_data[i_ob] is not None else None for i_ob in list(ob_data.keys())}
